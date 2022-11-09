04c00fbf0cb8f828fcc60bc91df6fe0c39ca90dd
basim <basims0077@gmail.com>
2022-11-09 12:22:03 +0530

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Banner from './components/Banner/Banner'
+import Head from './components/Header/Head'
+
+const Home = () => {
+  return (
+    <div>
+        <Head/>
+        <Banner/>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Header/Head.jsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import './Head.css'
+import { NavLink } from 'react-router-dom'
+
+const Head = () => {
+  return (
+    <div>
+      <div className="container-fluid">
+        <div className="row com-screen">
+          <div className="sub-com-screen">
+            <div className="logo">
+              <h4 className='fa-2x'>Srz.Esports.Org</h4>
+            </div>
+            <div className="com-nav ">
+              <div className="com-subnav ">
+                <li >
+                  <NavLink to={'/'}>Home</NavLink>
+                </li>
+                <li>
+                  <NavLink>About</NavLink>
+                </li>
+                <li>
+                  <NavLink>Teams</NavLink>
+                </li>
+                <li>
+                  <NavLink>Players</NavLink>
+                </li>
+                <li>
+                  <NavLink>Streamers</NavLink>
+                </li>
+              </div>
+            </div>
+          </div>
+
+
+          {/* Mobile - Screen */}
+
+          <div className="mob-nav">
+            <div className="sub-mob-nav">
+              <div className="mob-header">
+                <div className="mob-logo">
+                  <h5 className='fa-2x'>Srz.Esports.Org</h5>
+                </div>
+
+                <div className="mob-handbuger">
+                  <i class=" fa-solid fa-bars fa-3x" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"></i>
+                </div>
+              </div>
+
+
+              <div class="offcanvas offcanvas-end" tabindex={-1} id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+                <div class="offcanvas-header">
+                  <h5 id="offcanvasRightLabel " className='canvas-head fa-2x'>Srz.Esports.Org</h5>
+                  <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body canvas-body">
+
+                  <li>
+                    <NavLink to={'/'}>Home</NavLink>
+                  </li>
+                  <li>
+                    <NavLink>About</NavLink>
+                  </li>
+                  <li>
+                    <NavLink>Teams</NavLink>
+                  </li>
+                  <li>
+                    <NavLink>Players</NavLink>
+                  </li>
+                  <li>
+                    <NavLink>Streamers</NavLink>
+                  </li>
+                </div>
+              </div>
+
+
+
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  )
+}
+
+export default Head
